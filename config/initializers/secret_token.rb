cee5eb227b5ae16c4cd3ff35112e2b96f1a25919
Mike Villa <mike@gamekultur.com>
2011-03-08 10:28:35 -0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '32386ae17eefd0222fcbbc55cc2fa3c7e07efcee6cee76f6880cc80e0598d255ee16c62a090b84450683b039e46251422b4cc51f7cd6d284644ef420a0958020'
