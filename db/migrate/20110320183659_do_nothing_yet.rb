23d1b5c72277b86924d9847082cba6bdfa6a43d1
Mike Villa <mike@gamekultur.com>
2011-03-20 14:10:28 -0700

    Initial Migrations for users

---
+++ db/migrate/20110320183659_do_nothing_yet.rb
@@ -0,0 +1,9 @@
+class DoNothingYet < ActiveRecord::Migration
+  def self.up
+    # instructions for changing the DB to a new state
+  end
+
+  def self.down
+    # instructions for changing the DB back to the old state
+  end
+end
