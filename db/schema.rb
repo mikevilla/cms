7d5d8433c29f765b978c6ef632cab7d0a216992f
Mike Villa <mike@gamekultur.com>
2011-03-20 14:27:16 -0700

    Adding migration to change users table to admin_users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110320184324) do
+ActiveRecord::Schema.define(:version => 20110320211116) do
 
-  create_table "users", :force => true do |t|
-    t.string   "first_name", :limit => 25
-    t.string   "last_name",  :limit => 50
-    t.string   "email",                    :default => "", :null => false
-    t.string   "password",   :limit => 40
+  create_table "admin_users", :force => true do |t|
+    t.string   "first_name",      :limit => 25
+    t.string   "last_name",       :limit => 50
+    t.string   "email",           :limit => 100, :default => "", :null => false
+    t.string   "hashed_password", :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username",        :limit => 25
+    t.string   "salt",            :limit => 40
   end
+
+  add_index "admin_users", ["username"], :name => "index_admin_users_on_username"
 
 end
